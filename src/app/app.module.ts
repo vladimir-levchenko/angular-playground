c49409a026530332479bad4d88ac5c51b61e4921
Vlad Levchenko <vlevchenko@carelogistics.com>
2018-02-15 17:38:03 -0500

    Angular Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import {HeroesComponent} from './heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroService} from './hero.service';
+import {MessagesComponent} from './messages/messages.component';
+import {MessageService} from './message.service';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [HeroService],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
